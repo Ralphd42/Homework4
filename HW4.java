a7673e1c76768a4e7bfe92efbc198feaa89747de
Ralphd42 <rdonato@albany.edu>
2017-04-08 19:40:37 -0400

    Added test for Stack to stack transfer

--- HW4.java
+++ HW4.java
@@ -23,33 +23,39 @@ public class HW4 {
             //System.out.println("Working Directory = "
             //        + System.getProperty("user.dir"));
             System.out.println(args[0]);
-            
-            
+
             // Test Item 1 from Homework
             TextHeader("Item 1");
             Stack<String> TestStack1 = new Stack<>();
             Queue<String> TestQueue1 = new Queue<>();
-            int numAddded =FillStackFromFile(TestStack1, args[0]);
-            if(numAddded>0){
-                
-                FillQueueWithStack(TestStack1,TestQueue1);
-            }else
-            {
+            int numAddded = FillStackFromFile(TestStack1, args[0]);
+            if (numAddded > 0) {
+
+                FillQueueWithStack(TestStack1, TestQueue1);
+            } else {
                 TextHeader("Item 1 failed to load");
             }
             // Test Item 2 from Home work
             TextHeader("");
             TextHeader("Item 2");
-            numAddded = FillQueueFromFile( TestQueue1 ,args[0]);
-            if(numAddded>0){
-                
-                FillStackWithQueue(TestStack1,TestQueue1);
-            }else
-            {
+            numAddded = FillQueueFromFile(TestQueue1, args[0]);
+            if (numAddded > 0) {
+
+                FillStackWithQueue(TestStack1, TestQueue1);
+            } else {
                 TextHeader("Item 2 failed to load");
             }
-            
-            
+            TextHeader("");
+            TextHeader("Item 3");
+            numAddded = FillQueueFromFile(TestQueue1, args[0]);
+            Stack<String> Stack2 = new  Stack<>();
+            if (numAddded > 0) {
+
+                FillStackWithStack(TestStack1, Stack2);
+            } else {
+                TextHeader("Item 3 failed to load");
+            }
+
             //FillQueueFromFile(Queue,args[0]);
             //FillStackWithQueue(TestStack1,Queue);
             /*GenStack<String> gs = new GenStack();
@@ -108,14 +114,14 @@ public class HW4 {
         return retval;
 
     }
-    
-    public static void FillQueueWithStack(Stack<String> stack , Queue<String> queue){
+
+    public static void FillQueueWithStack(Stack<String> stack, Queue<String> queue) {
         TextHeader("Stack Contents(toString)");
         System.out.println(stack);
         TextHeader("Moving Stack to queue");
-        
-        do{
-            
+
+        do {
+
             String Item = stack.Pop();
             queue.Enqueue(Item);
             System.out.println("queuing " + Item);
@@ -124,26 +130,23 @@ public class HW4 {
         System.out.println(queue);
         TextHeader("Queue via dequing");
         boolean hasitems = true;
-        while (hasitems){
-            try{
+        while (hasitems) {
+            try {
                 System.out.println(queue.Dequeue());
-            }
-            catch(NoSuchElementException nse)
-            {
-                hasitems=false;
+            } catch (NoSuchElementException nse) {
+                hasitems = false;
             }
         }
-        
+
     }
-            
-    public static void FillStackWithQueue(Stack<String> stack , Queue<String> queue){
+
+    public static void FillStackWithQueue(Stack<String> stack, Queue<String> queue) {
         TextHeader("Queue Contents");
         System.out.println(queue);
         TextHeader("Moving Queue to Stack");
-        
-        
-        do{
-            
+
+        do {
+
             String Item = queue.Dequeue();
             stack.Push(Item);
             System.out.println("pushing " + Item);
@@ -152,22 +155,16 @@ public class HW4 {
         System.out.println(stack);
         TextHeader("Stack using pop");
         boolean hasitems = true;
-        while (hasitems){
-            try{
+        while (hasitems) {
+            try {
                 System.out.println(stack.Pop());
-            }
-            catch(NoSuchElementException nse)
-            {
-                hasitems=false;
+            } catch (NoSuchElementException nse) {
+                hasitems = false;
             }
         }
-        
-        
-        
-        
-        
+
     }
-    
+
     public static int FillQueueFromFile(Queue<String> queue, String FullFileName) {
         int retval = 0;
         File InputFile = new File(FullFileName);
@@ -191,16 +188,45 @@ public class HW4 {
 
     }
     
-    public static void TextHeader(String header){
+    public static void FillStackWithStack(Stack<String> stack, Stack<String> stackto) {
+        TextHeader("Stack Contents");
+        System.out.println(stack);
+        TextHeader("Moving Stack to new Stack");
+        
+        do {
+
+            String Item = stack.Pop();
+            stack.Push(Item);
+            System.out.println("pushing " + Item);
+        } while (!stack.isEmpty());
+        TextHeader("Stack  using(ToString)");
+        System.out.println(stackto);
+        TextHeader("Stack using pop");
+        boolean hasitems = true;
+        while (hasitems) {
+            try {
+                System.out.println(stackto.Pop());
+            } catch (NoSuchElementException nse) {
+                hasitems = false;
+            }
+        }
+
+    }
+    
+    
+    
+    
+    
+
+    public static void TextHeader(String header) {
         System.out.println();
         System.out.println(header);
         div();
-    
+
     }
-    public static void div()
-    {
+
+    public static void div() {
         System.out.println("----------------------------------");
     }
-    
-    
+
 }
